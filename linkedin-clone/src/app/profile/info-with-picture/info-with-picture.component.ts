ff4c1ccf66531f44626725ea05ceca9cac2d30fd
AlessandroGiorgino <alessandrogiorgino95@gmail.com>
2023-08-21 13:28:57 +0200

    inizio di data binding

--- linkedin-clone/src/app/profile/info-with-picture/info-with-picture.component.ts
+++ linkedin-clone/src/app/profile/info-with-picture/info-with-picture.component.ts
@@ -8,11 +8,14 @@ import { OnInit } from '@angular/core';
   styleUrls: ['./info-with-picture.component.scss'],
 })
 export class InfoWithPictureComponent {
+  infoUser: any = [];
   constructor(private srv: ServiceFetchService) {}
 
   ngOnInit(): void {
     this.srv.metodoPerGet().subscribe((res) => {
-      console.log(res);
+      // console.log(res);
+      this.infoUser.push(res);
+      console.log(this.infoUser);
     });
   }
 }
