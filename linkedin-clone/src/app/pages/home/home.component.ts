0083045d5f84c0c82b1f1dba3c8f0c833aeef898
AlessandroGiorgino <alessandrogiorgino95@gmail.com>
2023-08-24 11:26:44 +0200

    home

--- linkedin-clone/src/app/pages/home/home.component.ts
+++ linkedin-clone/src/app/pages/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { IPostForHome } from './../../models/postForHome';
+import { Component, OnInit } from '@angular/core';
+import { ServiceFetchService } from 'src/app/serviceFetch.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  posts!: IPostForHome[];
+  constructor(private srv: ServiceFetchService) {}
+  ngOnInit(): void {
+    this.srv.getPosts().subscribe((res) => {
+      this.posts = res.reverse();
+      this.posts = this.posts.slice(0, 55);
+      console.log(this.posts);
+    });
+  }
 }
